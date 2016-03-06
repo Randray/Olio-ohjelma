9ded727b1755f21a7a9e7f1d384575bee80f239e
Randray <jhfanta@gmail.com>
2016-03-06 15:21:42 +0200

    Added files via upload

---
+++ Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä_8._2
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+
+    }
+}
